c633605150c871509a9939b354666247f791964f
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-11-18 23:39:58 -0300

    feat: Start to implement register user route

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,29 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+	const createUserBodySchema = z.object({
+		name: z.string(),
+		email: z.string().email(),
+		password: z.string().min(6),
+	})
+
+	const { name, email, password } = createUserBodySchema.parse(request.body)
+
+	try {
+		const prismaUsersRepository = new PrismaUsersRepository()
+		const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+		await registerUseCase.execute({ name, email, password })
+	} catch (err) {
+		if (err instanceof UserAlreadyExistsError) {
+			return reply.status(409).send({ message: err.message })
+		}
+
+		return reply.status(500).send()
+	}
+
+	return reply.status(201).send()
+}
