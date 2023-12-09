7b19253ceb31f05f8a6596f807f75667bc259ac7
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-12-09 11:22:47 -0300

    feat: Adds @fastify/jwt dependency to handle authentication

--- src/env.ts
+++ src/env.ts
@@ -2,15 +2,18 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-  PORT: z.coerce.number().default(3333)
+	NODE_ENV: z
+		.enum(['development', 'test', 'production'])
+		.default('development'),
+	PORT: z.coerce.number().default(3333),
+	JWT_SECRET: z.string(),
 })
 
 const result = envSchema.safeParse(process.env)
 
 if (!result.success) {
-  console.error('Invalid environment variables', result.error.format())
-  throw new Error('Invalid environment variables')
+	console.error('Invalid environment variables', result.error.format())
+	throw new Error('Invalid environment variables')
 }
 
 export const env = result.data
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -16,7 +16,17 @@ export async function authenticate(
 
 	try {
 		const authenticateUseCase = makeAuthenticateUseCase()
-		await authenticateUseCase.execute({ email, password })
+		const { user } = await authenticateUseCase.execute({ email, password })
+		const token = await reply.jwtSign(
+			{},
+			{
+				sign: {
+					sub: user.id,
+				},
+			},
+		)
+
+		return reply.status(200).send({ token })
 	} catch (err) {
 		if (err instanceof InvalidCredentialsError) {
 			return reply.status(401).send({ message: err.message })
@@ -24,6 +34,4 @@ export async function authenticate(
 
 		throw err
 	}
-
-	return reply.status(200).send()
 }
